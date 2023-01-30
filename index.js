3cf226ff05936e48f682f44223197e13fdae7aab
Agnes Rebane <u-279@tptlive.ee>
2023-01-30 14:55:44 +0200

    Add templating

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const nunjucks= require('nunjucks');
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('./index.njk');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('./about.njk');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('./contact.njk');
+});
+
+app.get('/gallery', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('./gallery.njk');
+});
+
+app.get('/values', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('./values.njk');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
--- readme.md
+++ readme.md
@@ -19,4 +19,4 @@
 
 `git clone` - ....link
 
-#heihei
+
